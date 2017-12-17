52005d359e7ee1ffd906693a89a88f5c10bde22d
P.Czatrowski <p.czatrowski@gmail.com>
2017-12-18 00:06:32 +0100

    Added custom text at the end of each post (to be rewritten using translations)

--- functions.php
+++ functions.php
@@ -14,4 +14,29 @@ function alx_scripts() {
 		if ( is_singular() && get_option( 'thread_comments' ) )	{ wp_enqueue_script( 'comment-reply' ); }
 	}
 
+/* Custom text at the end of each post
+/* ------------------------------------ */
+add_filter('the_content', 'append_content_text'); 
+function append_content_text($content)
+{
+    $currentlang = get_bloginfo('language');
+    if (!is_single())
+	{
+		return $content;
+    }
+    
+    if ($currentlang == "en-US")
+	{
+		$content .= "<hr><p><strong>You liked it? Stay with us!</strong></p><p>Follow us on <a href=\"http://facebook.com/tropimy\" target=\"_blank\"><strong>Facebook</strong></a>, or <a href=\"http://www.bloglovin.com/blog/12132223\" target=\"_blank\"><strong>Bloglovin</strong></a> or subscribe us on <a href=\"https://www.youtube.com/c/TroPiMy\" target=\"_blank\"><strong>Youtube</strong></a>!</div>";
+		return $content;
+    }
+    if ($currentlang == "pl-PL")
+	{
+		$content .= "<hr>";
+		$content .= "<p><strong>Jeśli spodobał Ci się ten tekst - zostań z nami!</strong></p>";
+		/*$content .= "<p>Polub nasz <a href=\"http://facebook.com/tropimy\" target=\"_blank\"><strong>fanpage na Facebooku</strong></a> lub <a href=\"http://www.bloglovin.com/blog/12132223\" target=\"_blank\"><strong>Bloglovin</strong></a>, zasubskrybuj nasz kanał na <a href=\"https://www.youtube.com/c/TroPiMy\" target=\"_blank\"><strong>Youtube</strong></a>!</div>";*/
+		$content .= do_shortcode('[yikes-mailchimp form="1" title="1" description="1" submit="Zapisz się!"]');
+		return $content;
+    }
+}
 ?>
