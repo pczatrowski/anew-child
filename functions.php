794378791c1d7a46529987dbfd53349998b2acef
P.Czatrowski <p.czatrowski@gmail.com>
2017-12-18 00:07:17 +0100

    fixing Google Webmaster Tools

--- functions.php
+++ functions.php
@@ -39,4 +39,16 @@ function append_content_text($content)
 		return $content;
     }
 }
+
+/* add hatom data - fix google wemaster tools errors missing author, entry-title, updated */
+function add_suf_hatom_data($content) {
+    $t = get_the_modified_time('F jS, Y');
+    $author = get_the_author();
+    $title = get_the_title();
+if (is_home() || is_singular() || is_archive() ) {
+        $content .= '<div class="hatom-extra" style="display:none;visibility:hidden;"><span class="entry-title">'.$title.'</span> was last modified: <span class="updated"> '.$t.'</span> by <span class="author vcard"><span class="fn">'.$author.'</span></span></div>';
+    }
+    return $content;
+    }
+add_filter('the_content', 'add_suf_hatom_data');
 ?>
